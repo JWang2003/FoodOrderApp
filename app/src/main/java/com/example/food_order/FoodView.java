73da4c0505b8c2794b636bcc52f961230bc49908
JWang0105 <jimmywangatschool@gmail.com>
2021-01-16 02:08:31 -0800

    FoodView.java is most likely finished

--- app/src/main/java/com/example/food_order/FoodView.java
+++ app/src/main/java/com/example/food_order/FoodView.java
@@ -2,8 +2,13 @@ package com.example.food_order;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,10 +20,15 @@ public class FoodView extends AppCompatActivity {
     Bitmap foodImage;
 
     //XML
-    
+    TextView dishName;
+    ImageView dishImage;
+    TextView dishQuantity;
+    TextView dishPrice;
 
-
-
+    //Buttons
+    Button increment;
+    Button decrement;
+    Button addToCart;
 
 
     @Override
@@ -28,6 +38,8 @@ public class FoodView extends AppCompatActivity {
 
         getIntents();
         connectXMLViews();
+
+        setButtonListeners();
     }
 
     public void getIntents() {
@@ -47,7 +59,40 @@ public class FoodView extends AppCompatActivity {
         System.out.println("Dish added: " + dish);
     }
 
-    public void connectXMLViews()  {
+    public void connectXMLViews() {
+        dishName = findViewById(R.id.food_name);
+        dishName.setText(dish.mFoodName);
+        dishImage = findViewById(R.id.food_image);
+        dishImage.setImageBitmap(foodImage);
+        dishPrice = findViewById(R.id.food_price);
+        dishPrice.setText(dish.mPrice);
+        dishQuantity = findViewById(R.id.food_quantity);
+        dishQuantity.setText(dish.mQuantity);
 
+        increment = findViewById(R.id.food_increment);
+        decrement = findViewById(R.id.food_decrement);
+        addToCart = findViewById(R.id.add_to_cart);
+    }
+
+    public void setButtonListeners() {
+
+        increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dish.mQuantity -= 1;
+            }
+        });
+        decrement.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dish.mQuantity += 1;
+            }
+        }));
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.addFoodToCart(dish, dish.mQuantity);
+            }
+        });
     }
 }
